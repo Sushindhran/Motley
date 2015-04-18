4bca29095024f91a54648e8b3820d8abee170085
Sushi Harikrishnan <sushi@neosavvy.com>
2015-04-18 17:03:02 -0400

    Prepping for data parsing.

--- server/lib/accumulator.js
+++ server/lib/accumulator.js
@@ -1,18 +1,5 @@
-var assert     = require('assert');
-var thrift     = require('thrift');
-var transport  = require('thrift/lib/thrift/transport');
-var ThriftHive = require('../lib/0.7.1-cdh3u2/ThriftHive');
+/**
+ * The accumulator pulls turnstile data from the mta website
+ * and parses, stores it in hive.
+ */
 
-// Client connection
-var options = {transport: transport.TBufferedTransport, timeout: 1000};
-var connection = thrift.createConnection('127.0.0.1', 10000, options);
-var client = thrift.createClient(ThriftHive, connection);
-// Execute query
-client.execute('show databases', function(err){
-	assert.ifError(err);
-	client.fetchAll(function(err, databases){
-		assert.ifError(err);
-		console.log(databases);
-		connection.end();
-	});
-});
